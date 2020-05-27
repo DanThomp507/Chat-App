d61f62340c91aa770c9281c1ddac08360cb0bd7a
Daniel Thompson <DanThomp507@gmail.com>
2020-05-27 18:35:48 -0400

    started to set up client side structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div>Hello World</div>
+        {/* <Route path='/' exact component={Join} />
+        <Route path='/chat' component={Chat} /> */}
+      </Router>
+    );
+  }
 }
 
 export default App;
