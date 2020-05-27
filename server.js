fa468b008109ae321b9e060150c073d7ca39f5c5
Daniel Thompson <DanThomp507@gmail.com>
2020-05-27 18:04:08 -0400

    renamed index.js to server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(cors());
+// app.use(router);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send(err.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and listening on ${PORT}`);
+});
