3d98635021b257cafcd6864d0dd6ac0e2d0a5508
Daniel Thompson <DanThomp507@gmail.com>
2020-06-23 11:22:09 -0400

    started to set up message components

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
-import io from "socket.io-client";
+import io from 'socket.io-client';
 
 import TextContainer from '../Text/Text';
 import InfoBar from '../InfoBar/Info'
