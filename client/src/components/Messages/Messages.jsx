269e2f32bd878a2de3d3b623ec5d67b4d6304242
Daniel Thompson <DanThomp507@gmail.com>
2020-06-23 17:19:22 -0400

    fixed error that prevented smooth scroll from working

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ScrollToBottom from 'react-scroll-to-bottom'
 import Message from '../Message/Message'
 
+import './messages.css';
+
 const Messages = ({ messages, name }) => (
-  <ScrollToBottom>
+  <ScrollToBottom className='messages'>
     {messages.map((message, i) =>
-        <Message key={i} message={message} name={name} />
+      <Message key={i} message={message} name={name} />
     )}
   </ScrollToBottom>
 )
