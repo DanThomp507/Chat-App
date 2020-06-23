eedb0b606dab48591dc597da98d8f21383f7e34b
Daniel Thompson <DanThomp507@gmail.com>
2020-06-23 10:19:01 -0400

    started to implement websocket logic on server side

--- server.js
+++ server.js
@@ -16,6 +16,22 @@ const io = socketio(server);
 app.use(cors());
 app.use(router);
 
+io.on('connect', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { user, error } = addUser({ id: socket.id, name, room })
+
+    if (error) return callback(error)
+
+    socket.join(user.room)
+    socket.emit('message', { user: 'admin', text: `Welcome to ${user.room}, ${user.name}` })
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined` })
+
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
+
+    callback()
+  })
+})
+
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).send(err.message);
