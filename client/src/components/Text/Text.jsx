7b7a2f320631ebdfbf87405e957f52455dc82033
Daniel Thompson <DanThomp507@gmail.com>
2020-06-23 10:42:15 -0400

    started to set up user interface

---
+++ client/src/components/Text/Text.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import {
+  Container
+} from './text.styles'
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+const TextContainer = ({ users }) => (
+  <Container>
+    {users
+      ? (
+        <div>
+          <h1>People currently chatting:</h1>
+          <div className='activeContainer'>
+            <h2>
+              {users.map(({ name }) => (
+                <div key={name} className='activeItem'>
+                  {name}
+                  <img alt='Online Icon' src={onlineIcon} />
+                </div>
+              ))}
+            </h2>
+          </div>
+        </div>
+      )
+      : null
+    }
+  </Container>
+)
+
+export default TextContainer
---
+++ client/src/components/Text/text.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { device } from '../../utils/breakpoints'
+
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+margin-left: 100px;
+color: white;
+height: 60%;
+justify-content: space-between;
+`
